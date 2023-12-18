27232aa9d127f8de72182a8efed386de961ace35
Belal-Alchahrour <belal2005alchahrour000@gmail.com>
2023-12-18 17:48:38 -0600

    Added getter methods and distance calculation for Limelight

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -4,6 +4,9 @@
 
 package frc.robot.subsystems;
 
+import org.ejml.data.DMatrixRMaj;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -12,11 +15,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Limelight extends SubsystemBase {
   NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-  NetworkTableEntry tv = table.getEntry("tv"); //Whether the limelight has any valid targets (0 or 1)
+  NetworkTableEntry tv = table.getEntry("tv"); //Whether the limelight has any valid targets (0 if no target; 1 if target)
   NetworkTableEntry tx = table.getEntry("tx"); //Horizontal Offset From Crosshair To Target
   NetworkTableEntry ty = table.getEntry("ty"); //Vertical Offset From Crosshair To Target
   NetworkTableEntry ta = table.getEntry("ta"); //Target Area
   NetworkTableEntry tid = table.getEntry("tid"); //ID of the primary in-view AprilTag
+
+  NetworkTableEntry robotPoseTargetSpace = table.getEntry("botpose_targetspace");
+  NetworkTableEntry cameraPoseTargetSpace = table.getEntry("camerapose_targetspace");
 
   // Reads values periodically
   double v = tv.getDouble(0);
@@ -27,6 +33,8 @@ public class Limelight extends SubsystemBase {
 
   double angleToGoalRadians = y * (3.14159/180); //vertical angle from camera to April Tag in Radians
 
+  double distanceToTarget = 0;
+  double xToTarget, yToTarget, rotToTarget;
 
   /** Creates a new Limelight. */
   public Limelight() {
@@ -41,10 +49,42 @@ public class Limelight extends SubsystemBase {
     SmartDashboard.putNumber("LimelightV", v);
     SmartDashboard.putNumber("LimelightA", a);
     SmartDashboard.putNumberArray("LimelightID", id);
+
+    double[] botPositionArray = robotPoseTargetSpace.getDoubleArray(new double[6]);
+    double[] camPositionArray = cameraPoseTargetSpace.getDoubleArray(new double[6]);
+
+    xToTarget = botPositionArray[0];
+    yToTarget = botPositionArray[2]; //This is distance in z-axis of limelight
+    rotToTarget = camPositionArray[4];
+
+    distanceToTarget = Math.hypot(xToTarget, yToTarget); //Distance from robot to April Tag
+
   }
 
-  public double estimateDistance() {
-    double distanceFromLimelightToGoal = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
-    return distanceFromLimelightToGoal;
+  // public double estimateDistance() {
+  //   double distanceFromLimelightToGoal = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
+  //   return distanceFromLimelightToGoal;
+  // }
+
+  public double getDistance() {
+    return distanceToTarget;
+  }
+
+  public double getXDistance() {
+    return xToTarget;
+  }
+
+  public double getYDistance() {
+    return yToTarget;
+  }
+
+  public Rotation2d getRotationToTargetPlan() {
+    return Rotation2d.fromDegrees(rotToTarget);
+  }
+
+  public boolean getIsDetecting() {
+    int id = (int) tid.getInteger(Integer.MAX_VALUE);
+    boolean isDetecting = tid.getInteger(0) == 1 && id >= 1 && id <= 8;
+    return isDetecting;
   }
 }
